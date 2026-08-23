0733ed605c5dd5ca7da68a635c2ccf4062f4c247
Piotr Paluch <400luxluvr@gmail.com>
2026-08-24 01:46:56 +0200

    Make source helper methods private

--- source.py
+++ source.py
@@ -14,7 +14,7 @@ class HimalayasSource(Source):
 
     def fetch(self) -> list[dict]:
         try: 
-            response = self.fetch_url()
+            response = self._fetch_url()
         except requests.RequestException as e: 
             match e: 
                 case requests.HTTPError(): 
@@ -29,7 +29,7 @@ class HimalayasSource(Source):
             return []
     
         try:
-            items = self.extract_items(response)
+            items = self._extract_items(response)
         except ET.ParseError:
             print(f"Could not parse XML from URL: {self.url}")
             return []
@@ -40,46 +40,39 @@ class HimalayasSource(Source):
         jobs = []
         for item in items:
             try:
-                jobs.append(self.item_to_job(item, self.namespaces))
+                jobs.append(self._item_to_job(item))
             except ValueError as e:
                 print(f"Skipping item: {e}")
 
         return jobs
 
-    def fetch_url(self) -> requests.Response:
+    def _fetch_url(self) -> requests.Response:
         res = requests.get(self.url, timeout=10)
         res.raise_for_status()
         return res
 
-    def extract_items(self, response: requests.Response) -> list[ET.Element]:
-            root = ET.fromstring(response.content)
+    def _extract_items(self, response: requests.Response) -> list[ET.Element]:
+        root = ET.fromstring(response.content)
 
-            channel = root.find("channel")
-            if channel is None:
-                raise ValueError("RSS feed is missing channel")
-            return channel.findall("item")
+        channel = root.find("channel")
+        if channel is None:
+            raise ValueError("RSS feed is missing channel")
+        return channel.findall("item")
 
-    def item_to_job(self, item: ET.Element, namespaces: dict) -> dict:
-            link = item.findtext("link")
+    def _item_to_job(self, item: ET.Element) -> dict:
+        link = item.findtext("link")
 
-            if link is None:
-                raise ValueError("Job is missing link")
-            
-            return  {
-                "title": item.findtext("title", default="Unknown"),
-                "description": item.findtext("description", default="Unknown"),
-                "link": link,
-                "pubDate": item.findtext("pubDate", default="Unknown"),
-                "company": item.findtext(
-                    "himalayas:companyName",
-                    default="Unknown",
-                    namespaces=namespaces,
-                ),
-            } 
-
-if __name__ == "__main__":
-    source = HimalayasSource("https://himalayas.app/jobs/rss")
-    jobs = source.fetch()
-
-    print(f"Fetched {len(jobs)} jobs")
-    print(jobs[:1])
+        if link is None:
+            raise ValueError("Job is missing link")
+        
+        return  {
+            "title": item.findtext("title", default="Unknown"),
+            "description": item.findtext("description", default="Unknown"),
+            "link": link,
+            "pubDate": item.findtext("pubDate", default="Unknown"),
+            "company": item.findtext(
+                "himalayas:companyName",
+                default="Unknown",
+                namespaces=self.namespaces,
+            ),
+        } 

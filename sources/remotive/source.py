cb1baa5ec6dc3c2f2e9f6eba1b2974c21f9b8efa
Piotr Paluch <400luxluvr@gmail.com>
2026-09-09 19:34:28 +0200

    feat: add Remotive error handling

--- sources/remotive/source.py
+++ sources/remotive/source.py
@@ -1,9 +1,13 @@
+import logging
+
 import requests
 
+from exceptions import FetchError, ParserError
 from job import Job
 from sources.remotive.parser import RemotiveParser
 from sources.source import Source
 
+logger = logging.getLogger(__name__)
 
 class RemotiveSource(Source):
     def __init__(self, url: str, timeout: int):
@@ -12,9 +16,33 @@ class RemotiveSource(Source):
         self.parser = RemotiveParser()
 
     def fetch(self) -> list[Job]:
-        response = self._fetch_url()
-        data = response.json()
-        return self.parser.parse(data)
+        try:
+            response = self._fetch_url()
+        except requests.RequestException as e:
+            match e:
+                case requests.HTTPError():
+                    msg = f"Fetching failed for URL: {self.url}"
+                case requests.Timeout():
+                    msg = f"Fetching took too long for URL: {self.url}"
+                case requests.ConnectionError():
+                    msg = f"Could not reach the network for URL: {self.url}"
+                case _:
+                    msg = f"Unexpected request error for URL: {self.url}"
+            logger.error("%s | Details: %s", msg, e)
+            raise FetchError(msg) from e
+
+        try:
+            data = response.json()
+        except requests.exceptions.JSONDecodeError as e:
+            msg = f"Invalid JSON response from URL: {self.url}"
+            logger.error("%s | Details: %s", msg, e)
+            raise FetchError(msg) from e
+
+        try:
+            return self.parser.parse(data)
+        except ParserError as e:
+            logger.error("Invalid job data from URL: %s: %s", self.url, e)
+            raise
 
     def _fetch_url(self) -> requests.Response:
         res = requests.get(self.url, timeout=self.timeout)
